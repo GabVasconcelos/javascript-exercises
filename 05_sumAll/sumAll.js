77cd5e51204215b55ae672b1b04d63d5e5ab6ed5
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-23 23:46:45 -0300

    Solve fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    args.sort()
+    let result = 0
+    let start = args[0]
+    let end = args[1]
+    
+    if (typeof start != "number" || typeof end != "number") return "ERROR"
+    if (start < 0 || end < 0) return "ERROR"
 
+    for (let i = start; i<=end; i++) {
+        result += i
+    }
+
+    return result
 };
 
 // Do not edit below this line
