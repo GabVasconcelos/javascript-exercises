69d72125018eb44316b8098e149a8a57739d31a4
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-23 23:58:50 -0300

    Solve seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,10 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return Number(((temp - 32) * 5/9).toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Number(((temp * 9/5) + 32).toFixed(1))
+  //             ^ first set of parenthesis not really needed but here for consistency
 };
 
 // Do not edit below this line
