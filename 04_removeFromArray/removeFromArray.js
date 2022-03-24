9ad0dd3f38a4aec1b4d5a1068c395babb4353afe
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-24 00:04:17 -0300

    Add alternative solution (commented out)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -5,6 +5,12 @@ const removeFromArray = function(arr, ...values) {
         arr.splice(index, 1)
     }
     return arr
+
+    // more elegant solution: 
+    // var removeFromArray = function(...args) {
+    //   const array = args[0]
+    //   return array.filter(val => !args.includes(val))
+    // }
 };
 
 // Do not edit below this line
