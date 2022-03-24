6a55938b073d374ebb5f1647afc20927ba2ca402
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-23 22:59:34 -0300

    Solve second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(str, num=1) {
+    let ret = ""
+    for (let i=1; i<=num; i++) {
+        ret += str
+    }
+    return num < 0 && "ERROR" || ret 
 };
 
 // Do not edit below this line
