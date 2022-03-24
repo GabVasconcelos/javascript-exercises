20129648c82c07975fc21420b3f50f626bc183a1
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-03-23 23:03:12 -0300

    Solve third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let characters = str.split("")
+    let returning = ""
+    for (chr of characters) {
+        returning = chr + returning
+    }
+    return returning
 };
 
 // Do not edit below this line
