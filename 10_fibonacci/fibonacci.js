9c3f572391fdebc115af50b6dbabdb90ade0010a
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-04-08 15:28:39 -0300

    Horribly solve tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    let fib = [0, 0, 1] // what is this
+    position = Number(position)
+    if (position<0) return "OOPS"
+    for (let i=1; i<=position+1; i++) {
+        fib[i] = fib[i] || ((fib[i-1] || 0) + (fib[i-2] || 0))
+    }
+    return fib[position+1]
+    // this is probably the worst algorithm i have ever done
 };
 
 // Do not edit below this line
