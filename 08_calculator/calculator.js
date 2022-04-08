08041ae2f0628342006393a4611abccc22b34634
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-04-08 14:50:09 -0300

    Solve eighth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...numbers) {
+	let result = [...numbers].reduce((total, num) => total + num, 0)
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((total, num) => total + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+	return numbers.reduce((total, num) => total * num, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	function f(n) {
+		return n>1 && f(n-1)*n || 1
+	}
+	return f(num)
 };
 
 // Do not edit below this line
