bdee52ff59e7c4902d1a1ba15a292196d65fb2a7
GabVasconcelos <gabrielvasconcelos2709@gmail.com>
2022-04-08 15:13:17 -0300

    Solve ninth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const letters = word.replace(/[^a-zA-Z]+/g, "").split("")
+                                // ok ^
+    return letters.every((letter, index) => letters[letters.length-1-index].toLowerCase() == letter.toLowerCase())
 };
 
 // Do not edit below this line
